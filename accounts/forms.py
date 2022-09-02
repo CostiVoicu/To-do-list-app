ab1d171c9626a9d9c188330e366967d6dfd8ca94
Costi Voicu <costivoicu12@gmail.com>
2022-09-02 17:37:14 +0300

    Added login view

---
+++ accounts/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class UserForm(AuthenticationForm):
+    username =  forms.CharField( max_length=50, widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'myfieldclass'}))
+
+# class UserForm(forms.Form):
+#     username = forms.CharField(label='Username', max_length=50 ,widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'myfieldclass'}))
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
 def login_view(request):
-    context = {}
+    if request.method == 'POST':
+        form = UserForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserForm(request)
+    context = {
+        'form': form
+    }
+
     return render(request, 'accounts/login.html', context=context)
+
+# def login_view(request):
+#     context = {}
+#     if request.method == 'POST':
+#         form = UserForm(request.POST)
+#         if form.is_valid():
+#             username = form['username'].value()
+#             password = form['password'].value()
+#             user = authenticate(username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('/')
+#             else:
+#                 messages.error(request, 'username or password not correct')
+#                 return redirect('/login/')
+#     else:
+#         form = UserForm()
+#     context['form'] = form
+#     return render(request, 'accounts/login.html', context=context)
 
 def logout_view(request):
     context = {}
+    logout(request)
     return render(request, 'accounts/logout.html', context=context)
 
 def register_view(request):
