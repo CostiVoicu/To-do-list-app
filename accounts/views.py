dfe2ee05d966e3f1074e522f5cb6228277efd9c6
Costi Voicu <costivoicu12@gmail.com>
2022-09-08 16:09:35 +0300

    Added register functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .forms import UserForm
 
 # Create your views here.
@@ -39,12 +40,15 @@ def login_view(request):
 #     return render(request, 'accounts/login.html', context=context)
 
 def logout_view(request):
-    context = {}
     if request.method == 'POST':
         logout(request)
         return redirect('/')
-    return render(request, 'accounts/logout.html', context=context)
+    return render(request, 'accounts/logout.html', {})
 
 def register_view(request):
-    context = {}
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {'form': form}
     return render(request, 'accounts/register.html', context=context)
